4eab12e7ad4d2b9d8f153f298aa47558bcfdb796
Xushnud Eshtemirov <xushnud122997@gmail.com>
2024-12-17 09:24:10 +0500

    Finished

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,38 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
+        Profile profile = new Profile();
+        try {
+            InputStream input = new FileInputStream(file);
+            StringBuilder content = new StringBuilder();
+
+            int i;
+            while ((i = input.read()) != -1) {
+                content.append((char) i);
+            }
+
+            String str = content.toString();
+            String[] s = str.split("\\s+");
+
+            profile.setName(s[1]);
+            profile.setAge(Integer.valueOf(s[3]));
+            profile.setEmail(s[5]);
+            profile.setPhone(Long.valueOf(s[7]));
+
+            input.close();
+            return profile;
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+
         return new Profile();
     }
 }
